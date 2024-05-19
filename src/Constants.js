e94f06b007ec8fadfefd62c09be84af9391646d4
Deep13 <deep.deepak30@gmail.com>
2024-05-19 23:09:14 +0530

    initial

---
+++ src/Constants.js
@@ -0,0 +1,130 @@
+export const day_title = 'Happy Holi';
+export const banner_key1 = 'ca-app-pub-4717579333914549/9398748183';
+export const banner_key2 = 'ca-app-pub-4717579333914549/5822110036';
+export const banner_key3 = 'ca-app-pub-4717579333914549/3195946691';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/1882865021';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/3658787605';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/5630538340';
+export const language = ['English', 'Hindi'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'May the spirit of Holi bring you joy,The gladness of Holi give you Hope, The warmth of Holi grant you cheer.\nWish You A Very Happy Holi',
+      },
+      {
+        id: '002',
+        text:
+          'Let the colors of Holi spread happiness, gaeity, peace and love all around.\nWish you a blessed Holi!',
+      },
+      {
+        id: '003',
+        text:
+          'May the splash of colors bring joy within your family.\nHave a Happy Holi!',
+      },
+      {
+        id: '004',
+        text:
+          'Happiness is the most beautiful color in life which I wish should stay forever with you.\nHappy Holi!',
+      },
+      {
+        id: '005',
+        text:
+          'May God gift you all the colours of life, colours of joy, colours of happiness, colours of friendship, colours of love and all other colours you want to paint your life in. \nHappy Holi.',
+      },
+      {
+        id: '006',
+        text: 'May this Holi add more colors to your life.\nHappy Holi.',
+      },
+      {
+        id: '007',
+        text:
+          'Wishing you a Holi full of fun and frolic.Eat gujiyas, dance like crazy.Enjoy.\nHappy Holi!',
+      },
+      {
+        id: '008',
+        text:
+          'Enjoy every colour of Holi and u will get every happiness u desire.Hey, God is always with u.Enjoy every moment.\nHappy Holi!!',
+      },
+      {
+        id: '009',
+        text:
+          'Wishing you and your family a very bright, colourful and joyful Holi.\nWith love and best wishes',
+      },
+      {
+        id: '010',
+        text:
+          'Happy Holi to you and your family. We wish your health, prosperity and business achievements at this prismic colour eve. May Allah bless you with all his mercies! Aamin. \nHappy Holi!!!',
+      },
+      {
+        id: '011',
+        text:
+          "'Bright colors, water balloons, lavish gujiyas and melodious songs' are the ingredients of perfect Holi. Wish you a very happy and wonderful Holi.",
+      },
+    ]
+  }
+  ,
+  Hindi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'वो गुलाल की ठंडक, वो शाम की रोनक, वो लोगों का गाना, वो गलियों का चमकना, वो दिन में मस्ती, वो रंगों की धूम, होली आ गई है होली है \n Happy Holi',
+      },
+      {
+        id: '002',
+        text:
+          'खुशियों से हो ना कोई दुरी, रहे न कोई ख्वाहिश अधूरी, रंगो से भरे इस मौसम में, रंगीन हो आपकी दुनिया पूरी\nहैप्पी होली ',
+      },
+      {
+        id: '003',
+        text:
+          'मक्के की रोटी निम्बू का अचार, सूरज की किरणें खुशियों की बहार, चाँद की चांदनी अपनों का प्यार, मुबारक हो आपको होली का त्यौहार',
+      },
+      {
+        id: '004',
+        text:
+          'आपको और आपके परिवार को होली की खुब सारी शुभकामनाये, इसी दुआ के साथ आपके व आपके परिवार के साथ सभी के लिए सुखदायक, मंगलकारी व आन्नददायक हो।\nहोली की खुब सारी शुभकामनाये…',
+      },
+      {
+        id: '005',
+        text:
+          'रंग के त्यौहार में, सभी रंगों की हो भरमार, ढेर सारी खुशियों से भरा हो आपका संसार, यही दुआ है हमारी भगवान से हर बार।',
+      },
+      {
+        id: '006',
+        text:
+          'प्यार के रंगों से भरो पिचकारी, स्नेह के रंगों से रंग दो दुनिया सारी, ये रंग न जाने न कोई जात न बोली, सबको हो मुबारक ये हैप्पी होली',
+      },
+      {
+        id: '007',
+        text:
+          'गुल ने गुलशन से गुलफान भेजा है, सितारों ने आसमान से सलाम भेजा है, मुबारक हो आप को होली का त्यौहार, हमने दिल से ये पैगाम भेजा है',
+      },
+      {
+        id: '008',
+        text:
+          'अपनों से अपनों को मिलाती है होली, खुशियों के रंग लाती है होली, बरसों से बिछड़ें हैं जो उन सबको मिलाती है होली, मेरी तरफ से आप सबको हैप्पी होली',
+      },
+      {
+        id: '009',
+        text:
+          'कदम कदम पर खुशियां रहें, गम से कभी ना हो सामना, जिंदगी में हर पल खुशियां नसीब हों\nमेरी तरफ से होली की शुभकामना',
+      },
+      {
+        id: '010',
+        text:
+          'रंग उड़ाए पिचकारी, रंग से रंग जाये दुनिया सारी, होली का रंग आपके जीवन को रंग दे\nये शुभकामना है हमारी',
+      },
+      {
+        id: '011',
+        text:
+          "हमेशा मीठी रहे आपकी बोली, खुशियों से भर जाये आपकी झोली, आप सबको मेरी तरफ से 'हैप्पी होली'\nWishing Ur a Happy Holi to All 2020",
+      },
+    ]
+  }
+
+
+};
